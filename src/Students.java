1ffd7f6a0bf5303d67d7d0f58264d4139f339711
Santhosh3D2Y <adimulamsanthosh007@gmail.com>
2026-07-02 17:10:55 +0530

    completed ver

--- src/Students.java
+++ src/Students.java
@@ -1,102 +1,140 @@
-import java.util.Scanner;
 import java.sql.*;
- class Management{
-    public static void insert(int pid,String name,int age,String gender) {
-           try{  
-        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/students","root","");
-        Statement stmt=con.createStatement();
-        stmt.execute("insert into patient values("+pid+",'"+name+"',"+age+",'"+gender+"');");
-        con.close();
-         System.out.println("Inserted record sucessfully");
-        }catch(Exception exp){
-            System.out.println("Error:"+exp);
+import java.util.Scanner;
+
+class PatientManager {
+    public static void insert() {
+        Scanner s = new Scanner(System.in);
+        try{
+            Connection con=DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/apolohospital","root","");
+            Statement st = con.createStatement();
+            System.out.println("enter id, name, age, gender ");
+            int pid = s.nextInt();
+            String name = s.next();
+            int age = s.nextInt();
+            String gender = s.next();                        
+            st.execute("insert into patient values("+pid+", '"+name+"', "+age+", '"+gender+"') ");
+            // st.execute("update patient set age=19 where pid=3");
+            // st.execute("delete from patient where pid=2");
+
+            con.close();
+            System.out.println("operation Success!");
+        }
+        catch(Exception exp) 
+        {
+            System.out.println("operation Failed!" + exp);
+        }        
+    }
+    public static void update() {
+        Scanner s=new Scanner(System.in);
+        try{
+            Connection con=DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/apolohospital","root","");
+            Statement st = con.createStatement();
+            
+            // st.execute("insert into patient values(5, 'steve', 30, 'male') ");
+            System.out.println("enter age to modify ");
+            int age = s.nextInt();
+            System.out.println("enter pid ");
+            int pid = s.nextInt();
+
+            st.execute("update patient set age="+age+" where pid="+pid);
+            // st.execute("delete from patient where pid=2");
+
+            con.close();
+            System.out.println("operation Success!");
+        }
+        catch(Exception exp) 
+        {
+            System.out.println("operation Failed!" + exp);
         }
     }
-     public static void delete() {
-        try{  
-        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/students","root","");
-        Statement stmt=con.createStatement();
-        System.out.println("Enter id to delete:");
-         Scanner sc=new Scanner(System.in);
-        int pid=sc.nextInt();
-        stmt.execute("delete from patient where pid="+pid+";");
-        con.close();
-         System.out.println("Deleted record sucessfully");
-        }catch(Exception exp){
-            System.out.println("Error:"+exp);
+    public static void remove() {
+        Scanner s=new Scanner(System.in);
+        try{
+            Connection con=DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/apolohospital","root","");
+            Statement st = con.createStatement();
+
+            System.out.println("enter pid of patient to remove :");
+            int pid = s.nextInt();
+            
+            // st.execute("insert into patient values(5, 'steve', 30, 'male') ");
+            // st.execute("update patient set age=19 where pid=3");
+            st.execute("delete from patient where pid="+pid);
+
+            con.close();
+            System.out.println("operation Success!");
         }
+        catch(Exception exp) 
+        {
+            System.out.println("operation Failed!" + exp);
+        }        
     }
-     public static void update() {
-        try{  
-        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/students","root","");
-        Statement stmt=con.createStatement();
-        System.out.println("Enter id to update:");
-        System.out.println("Enter age(modified):");
-         Scanner sc=new Scanner(System.in);
-        int pid=sc.nextInt();
-        int age=sc.nextInt();
-        stmt.execute("update patient set age="+age+" where pid="+pid+")");
-        con.close();
-         System.out.println("Updated record sucessfully");
-        }catch(Exception exp){
-            System.out.println("Error:"+exp);
+    public static void view() {
+        try{
+            Connection con=DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/apolohospital","root","");
+            Statement st = con.createStatement();
+            
+            // st.execute("insert into patient values(5, 'steve', 30, 'male') ");
+            // st.execute("update patient set age=19 where pid=3");
+            // st.execute("delete from patient where pid=2");
+            ResultSet rs = st.executeQuery("select * from patient");
+
+            while(rs.next())
+                System.out.println(
+                    rs.getInt("pid")+" "+
+                    rs.getString("name")+" "+
+                    rs.getInt("age")+" "+
+                    rs.getString("gender"));
+
+            con.close();
+            System.out.println("operation Success!");
         }
-    }
-     public static void display() {
-        try{  
-        Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/students","root","");
-        Statement stmt=con.createStatement();
-        ResultSet rs=stmt.executeQuery("select*from patient;");
-        while (rs.next()) {
-            System.out.println(rs.getInt("pid")+""+rs.getString("name")+""+rs.getInt("age")+""+rs.getString("gender"));
-        }
-        con.close();
-         System.out.println("data displayed");
-        }catch(Exception exp){
-            System.out.println("Error:"+exp);
+        catch(Exception exp) 
+        {
+            System.out.println("operation Failed!" + exp);
         }
 
-
     }
+
 }
-
-
 public class Students {
-    public static void menu(){
-   System.out.println("1.Insert");
-   System.out.println("2.Delete");
-   System.out.println("3.Update");
-   System.out.println("5.Display");
-   System.out.println("6.quit");
-}
-    public static void main(String[] args) throws Exception {
-         Scanner sc=new Scanner(System.in);
-        while (true) {
-            System.out.println("Student management system");
-            menu();
-            int choice=sc.nextInt();
-            switch (choice) {
-                case 1:
-                Management.insert();
-                    break;
-                 case 2:
-                    Management.delete();
-                    break;
-                 case 3:
-                    Management.update();
-                    break;
-                 case 5:
-                    Management.display();
-                    break;
-                 case 6:
-                    return;
-            
-                default:
-                    System.out.println("no operation is choosen");
-                    break;
-                }
+    // 1. insert row in db table
+    // 2. fetch rows from db tables
 
-            
-        }
+    public static void menu(){
+        System.out.println("1. Insert ");
+        System.out.println("2. Update ");
+        System.out.println("3. Delete ");
+        System.out.println("4. View ");
+        System.out.println("0. Quit ");
+        System.out.println("Enter choice ");
     }
+    public static void main(String[]args) {
+        Scanner s = new Scanner(System.in);
+        while(true)
+        {
+            System.out.println("Patient Management System ");
+            menu();
+            int choice = s.nextInt();
+            switch(choice)
+            {
+                case 0: return;
+                case 1: 
+                    PatientManager.insert();
+                    break;
+                case 2:
+                    PatientManager.update();
+                    break;
+                case 3:
+                    PatientManager.remove();
+                    break;
+                case 4:
+                    PatientManager.view();
+                    break;
+            }
+        }
+    }    
 }
